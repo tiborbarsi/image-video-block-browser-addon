38e1d83d9b3a43925283aa94780b95fc80429a09
Tibor Barsi <tiborbarsi@outlook.com>
2016-09-15 18:45:58 +0200

    Initial Commit Image Video Block

---
+++ src/options/options.js
@@ -0,0 +1,40 @@
+// options.js
+
+
+var $ = document.querySelector.bind(document);
+
+
+/* options */
+var options = {
+  els: {
+    imgBlock: $('input#imgBlock'),
+    videoHide: $('input#videoHide'),
+    svgHide: $('input#svgHide'),
+    canvasHide: $('input#canvasHide')
+  },
+
+  get: function() {
+    var self = this;
+    chrome.storage.local.get(function(data) {
+      self.els.imgBlock.checked = data.imgBlock;
+      self.els.videoHide.checked = data.videoHide;
+      self.els.svgHide.checked = data.svgHide;
+      self.els.canvasHide.checked = data.canvasHide;
+    });
+  },
+  set: function(e) {
+    var data = {};
+    data[e.target.id] = e.target.checked;
+    chrome.storage.local.set(data);
+  },
+  init: function() {
+    this.get();
+    chrome.storage.onChanged.addListener(this.get.bind(this));
+
+    for (var el in this.els)
+      this.els[el].addEventListener('change', this.set);
+  }
+};
+
+
+options.init();
