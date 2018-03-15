f43759c9312247dfd9091d876705a4e8d549457c
Tibor Barsi <tiborbarsi@outlook.com>
2018-03-16 00:48:07 +0100

    refactor: background.js

--- src/background/background.js
+++ src/background/background.js
@@ -2,102 +2,98 @@
 // jshint esversion: 6
 
 
-/* Context Menus */
-function onCreated() {
-  if (browser.runtime.lastError) {
-    console.log(`Error: ${browser.runtime.lastError}`);
+/* Image Request Blocker */
+class ImageRequestBlocker
+{
+  constructor()
+  {
+    this._imgBlock = false;
+
+    browser.storage.onChanged.addListener(this._updateState.bind(this));
+    browser.webRequest.onBeforeRequest.addListener(this._cancelRequest.bind(this),
+      {urls: ['<all_urls>'], types: ['image', 'imageset']}, ['blocking']);
+
+    this._updateState();  // initial
+  }
+
+  _updateState()
+  {
+    browser.storage.local.get().then(data => {
+      this._imgBlock = data.imgBlock;
+    });
+  }
+
+  _cancelRequest(details)
+  {
+    return {cancel: this._imgBlock};
   }
 }
 
-var contextMenus = {
-  menuItems: [
-    {  title: "Block Image", popupId: "imgBlock", checked: false },
-    {  title: "Hide Videos", popupId: "videoHide", checked: false },
-    {  title: "Hide Flash", popupId: "flashHide", checked: false },
-    {  title: "Hide Canvas", popupId: "canvasHide", checked: false },
-    {  title: "Hide SVGs", popupId: "svgHide", checked: false }
-  ],
 
-  createMenuItems: function() {
-    var self = this;
-    chrome.storage.local.get(function(data) {
-      for (var i in self.menuItems) {
-        var item = self.menuItems[i];
+/* Context Menu Handler */
+class ContextMenuHandler
+{
+  constructor()
+  {
+    this._menuItems = [
+      {title: browser.i18n.getMessage('imgBlockText'), popupId: 'imgBlock', checked: false},
+      {title: browser.i18n.getMessage('videoHideText'), popupId: 'videoHide', checked: false},
+      {title: browser.i18n.getMessage('flashHideText'), popupId: 'flashHide', checked: false},
+      {title: browser.i18n.getMessage('canvasHideText'), popupId: 'canvasHide', checked: false},
+      {title: browser.i18n.getMessage('svgHideText'), popupId: 'svgHide', checked: false},
+    ];
+
+    browser.storage.onChanged.addListener(this._updateCheckedState.bind(this));
+    browser.menus.onClicked.addListener(this._onMenuItemClick.bind(this));
+
+    this._createMenuItems();
+    this._updateCheckedState();
+  }
+
+  _createMenuItems()
+  {
+    browser.storage.local.get().then(data => {
+      for (let item of this._menuItems) {
         item.checked = data[item.popupId];
         browser.menus.create({
-          id: "menu#"+item.popupId,
-          type: "checkbox",
+          id: 'menu#' + item.popupId,
+          type: 'checkbox',
           title: item.title,
           checked: item.checked,
-          contexts: ['page', 'editable', 'frame', 'link', 'image', 'video'],
-        }, onCreated);
+          contexts: ['page', 'editable', 'frame', 'link', 'image', 'video']
+        });
       }
     });
+  }
 
-    // initialize check status
-    self.onStorageChange();
-  },
-
-  onStorageChange: function() {
-    var self = this;
-    chrome.storage.local.get(function(data) {
-      for (var i in self.menuItems) {
-        var item = self.menuItems[i];
-        if (item.checked == data[item.popupId]) {
+  _updateCheckedState()
+  {
+    browser.storage.local.get().then(data => {
+      for (let item of this._menuItems) {
+        if (item.checked == data[item.popupId])
           continue;
-        }
+
         item.checked = data[item.popupId];
-        browser.menus.update("menu#"+item.popupId, {
+        browser.menus.update('menu#' + item.popupId, {
           checked: data[item.popupId]
         });
       }
     });
-  },
+  }
 
-  onClick: function(info, tab) {
-    var self = this;
-
-    var data = {};
-    for (var i in this.menuItems) {
-      var item = this.menuItems[i];
-      if ((info.menuItemId == "menu#"+item.popupId)) {
+  _onMenuItemClick(info, tab)
+  {
+    let data = {};
+    for (let item of this._menuItems) {
+      if ((info.menuItemId == "menu#" + item.popupId)) {
         item.checked = !item.checked;
         data[item.popupId] = item.checked;
       }
     }
 
-    chrome.storage.local.set(data);
-  },
-
-  init: function() {
-    var self = this;
-    self.createMenuItems();
-    browser.menus.onClicked.addListener(self.onClick.bind(self));
-    chrome.storage.onChanged.addListener(self.onStorageChange.bind(self));
+    browser.storage.local.set(data);
   }
-};
-
-
-/* Blocker */
-var blocker = {
-  imgBlock: false,
-
-  get: function() {
-    var self = this;
-    chrome.storage.local.get(function(data) {
-      self.imgBlock = data.imgBlock;
-    });
-  },
-  init: function() {
-    this.get();
-    chrome.storage.onChanged.addListener(this.get.bind(this));
-
-    var self = this;
-    chrome.webRequest.onBeforeRequest.addListener(function(details) {
-      return {cancel: self.imgBlock};
-    }, {urls: ['<all_urls>'], types: ['image', 'imageset']}, ['blocking']);
-  }
-};
+}
 
 
 /* Keyboard Command Handler */
@@ -132,6 +128,6 @@ class OptionStateChanger
 
 
 // Init
-contextMenus.init();
-blocker.init();
+new ImageRequestBlocker();
+new ContextMenuHandler();
 new KeyboardCommandHandler();
